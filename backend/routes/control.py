70a43a26daff38cb508a00945720b69f032af8f1
Daniel Callero <hola@danicallero.es>
2025-08-15 12:50:05 +0200

    fix: handle ws from player with just one arg

--- backend/routes/control.py
+++ backend/routes/control.py
@@ -32,9 +32,11 @@ def notify_ws_clients_threadsafe(data):
     asyncio.run_coroutine_threadsafe(notify_ws_clients(data), main_loop)
 
 
-def notify_from_player(data, volume):
-    notify_ws_clients_threadsafe({"type": "now_playing", "now_playing": data})
-    notify_ws_clients_threadsafe({"type": "volume", "volume": volume})
+def notify_from_player(data: str = None, volume: int = None):
+    if (data != None):
+        notify_ws_clients_threadsafe({"type": "now_playing", "now_playing": data})
+    if (volume != None):
+        notify_ws_clients_threadsafe({"type": "volume", "volume": volume})
 
 
 class BaseTokenRequest(BaseModel):
